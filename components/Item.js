a08e5cc0ddb12e585f451de258db2312de369bba
MD Shimul <mdshemul480@gmail.com>
2021-08-25 16:25:10 +0600

    adding delete button

--- components/Item.js
+++ components/Item.js
@@ -3,7 +3,7 @@ import { View, Text, Box } from "native-base";
 import { StyleSheet } from "react-native";
 
 import moment from "moment";
-import { style } from "styled-system";
+
 const Item = () => {
   const time = Date.now();
   const formattedTime = moment(time).format("MMMM Do YYYY, h:mm a");
@@ -11,15 +11,25 @@ const Item = () => {
     <Box style={styles.card}>
       <View style={styles.detailArea}>
         {/* delete button */}
-        <View style={styles.deleteButton}></View>
+        <View style={styles.deleteButton}>
+          <Text>D</Text>
+        </View>
         <View>
-          <Text style={styles.productName} fontSize='2xl'>
+          <Text style={styles.leftSizeText} fontSize='2xl'>
             Coke
           </Text>
-          <Text style={styles.providerName} fontSize='md'>
+          <Text style={styles.leftSizeText} fontSize='md'>
             Feros
           </Text>
         </View>
+      </View>
+      <View>
+        <Text style={styles.rightSizeText} fontSize='2xl'>
+          ৳30
+        </Text>
+        <Text style={styles.rightSizeText} fontSize='md'>
+          {formattedTime}
+        </Text>
       </View>
     </Box>
   );
@@ -30,26 +40,31 @@ const styles = StyleSheet.create({
     margin: 10,
     backgroundColor: "#155e75",
     paddingVertical: 8,
-    borderRadius: 5,
+    borderRadius: 13,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 8,
   },
   detailArea: {
     flexDirection: "row",
     alignItems: "center",
   },
   deleteButton: {
-    width: 40,
-    height: 40,
+    // width: 40,
+    // height: 40,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
     borderRadius: 10,
     marginHorizontal: 8,
     borderColor: "#1f2937",
     backgroundColor: "#22d3ee",
     borderWidth: 3,
   },
-  productName: {
+  leftSizeText: { color: "#ecfeff" },
+  rightSizeText: {
     color: "#ecfeff",
-  },
-  providerName: {
-    color: "#ecfeff",
+    textAlign: "right",
   },
 });
 export default Item;
