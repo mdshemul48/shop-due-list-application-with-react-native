63e86c8af4b009845ddd7c01ab60c305ba3a4318
MD Shimul <mdshemul480@gmail.com>
2021-08-25 14:44:08 +0600

    adding total due area

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Heading, View, NativeBaseProvider} from 'native-base';
 
 const App = () => {
   return (
-    <View>
-      <Text>hello world</Text>
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.headerArea}>
+        <Heading style={styles.heading}>Total Due</Heading>
+        <Heading style={styles.heading}>৳100</Heading>
+      </View>
+    </NativeBaseProvider>
   );
 };
 
+const styles = StyleSheet.create({
+  headerArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+  },
+  heading: {
+    marginTop: 15,
+  },
+});
 export default App;
