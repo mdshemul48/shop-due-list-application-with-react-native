4f0eb2a3d039d526f88675503958fa44f22bc4ab
MD Shimul <mdshemul480@gmail.com>
2021-08-26 12:56:09 +0600

    adding input on change handler for all the input

--- components/Entry.js
+++ components/Entry.js
@@ -7,19 +7,29 @@ import { FormControl, Radio, Button } from "native-base";
 const Entry = () => {
   const [formState, setFormState] = useState({productName: "", price: "", option:"Feros" })
   
+  const productNameChangeHandler = (eventText) => {
+    setFormState(prevState=> ({...prevState, productName: eventText}))
+  }
+  const priceChangeHandler = (eventText) => {
+    setFormState(prevState=> ({...prevState, price: eventText}))
+  }
 
+  const optionChangeHandler = (eventText) => {
+    setFormState(prevState=> ({...prevState, option: eventText}))
 
+  }
+  console.log(formState)
   return (
     <KeyboardAvoidingView
       style={styles.createCard}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <FormControl style={styles.inputs}>
-        <TextInput placeholder='Product Name?' style={styles.productName} />
-        <TextInput placeholder='Price?' style={styles.Price} />
+        <TextInput value={formState.productName} onChangeText={productNameChangeHandler} placeholder='Product Name?' style={styles.productName} />
+        <TextInput value={formState.price} onChangeText={priceChangeHandler}  placeholder='Price?' style={styles.Price} />
       </FormControl>
       <FormControl>
-        <Radio.Group name='provider' style={styles.providers}>
+        <Radio.Group name='provider' style={styles.providers} onChange={optionChangeHandler} value={formState.option} >
           <Radio value='Feros' my={1}>
             Feros
           </Radio>
